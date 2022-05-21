d3da1903dd522d1b1c0b0837c2cb2e69ede7202e
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-05-22 00:34:36 +0900

    feat: open browser

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 
-// enquirer をインストール
 
-//　Select を require する
-
-// enquirer で実行する方法を調べる
-
-// enquirer で入力した内容を取得する
-
-// 取得した内容から url を作成する
-
-// clg で表示
 
 const { Form } = require('enquirer');
+
+// TODO(yamatatsu):  Search required installed packages
+const { platform } = require('os');
+const { exec } = require('child_process');
+
+const WINDOWS_PLATFORM = 'win32';
+const MAC_PLATFORM = 'darwin';
+const osPlatform = platform();
 
 const prompt = new Form({
   name: 'twesearch',
@@ -37,7 +35,16 @@ prompt.run()
     queries += ' min_retweets:' + value['min_retweets']
     queries += '&src=typed_query&f=top'
     const uri = baseUrl + queries
-    console.log(encodeURI(uri))
-    open(encodeURI(uri))
+    const url = encodeURI(uri)
+    console.log(url)
+
+    let command;
+    if (osPlatform === WINDOWS_PLATFORM) {
+      command = `start ${url}`;
+    } else if (osPlatform === MAC_PLATFORM) {
+      command = `open ${url}`;
+    }
+
+    exec(command);
   })
   .catch(console.error);
